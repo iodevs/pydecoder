527b3f3116081f4f900a637661b6b00e82dac6b9
Jindrich K. Smitka <jsmitka@smita.info>
2017-08-23 14:59:16 +0200

    Fixed array/list decoder

--- pydecoder/structs.py
+++ pydecoder/structs.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 '''Decoder structures'''
 
+from pyresult import is_error, value, ok
 from toolz import curry
 
 
 @curry
 def array(factory, vals):
     '''Decode string/value as list'''
-    return [factory(val) for val in vals]
+    result = []
+
+    for val in vals:
+        rv = factory(val)
+
+        if is_error(rv):
+            return rv
+
+        result.append(value(rv))
+
+    return ok(result)
--- tests/test_structs.py
+++ tests/test_structs.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring, redefined-outer-name, no-value-for-parameter
 
-from pyresult import is_ok, value
+from pyresult import is_ok, is_error
+from six import string_types
 from six.moves import zip
 
 from pydecoder.primitives import to_int
 from pydecoder.structs import array
 
 
-def test_array_returns_decoded_result_list():
+def test_array_returns_decoded_list_in_result():
     ins = ['1', '2', '3']
 
     rv = array(to_int, ins)
 
-    for orig, res in zip(ins, rv):
-        assert is_ok(res)
-        assert value(res) == int(orig)
+    assert is_ok(rv)
+
+    for orig, res in zip(ins, rv.value):
+        assert res == int(orig)
+
+
+def test_stop_on_first_error_and_returns_it():
+    ins = ['1', None, '3']
+
+    rv = array(to_int, ins)
+
+    assert is_error(rv)
+    assert isinstance(rv.value, string_types)
