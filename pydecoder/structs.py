9d583234d7267702750e9c7c747efb83137da32d
Jindrich K. Smitka <jsmitka@smita.info>
2017-09-25 18:07:24 +0200

    Fixed invalid values (eg. None) in array decoder

--- pydecoder/structs.py
+++ pydecoder/structs.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 '''Decoder structures'''
 
-from pyresult import is_error, value, ok
+from pyresult import is_error, value, ok, error
 from toolz import curry
 
 
 @curry
 def array(factory, vals):
-    '''Decode string/value as list'''
+    '''Decode string/value as list
+
+    array :: (a -> Result e b) -> List a -> Result e (List b)
+
+    >>> from pydecoder.primitives import to_int
+    >>> array(to_int, [1, 2, 3])
+    Result(status='Ok', value=[1, 2, 3])
+
+    >>> array(to_int, None)
+    Result(status='Error', value="'None' isn't list or tuple.")
+    '''
+    if not isinstance(vals, (list, tuple)):
+        return error('\'{}\' isn\'t list or tuple.'.format(vals))
+
     result = []
 
     for val in vals:
--- tests/test_structs.py
+++ tests/test_structs.py
@@ -5,12 +5,37 @@ from pyresult import is_ok, is_error
 from six import string_types
 from six.moves import zip
 
+from hypothesis import given
+from hypothesis.strategies import (
+    lists,
+    one_of,
+    none,
+    integers,
+    recursive,
+    text,
+    booleans,
+    floats,
+    complex_numbers,
+    dictionaries
+)
+
 from pydecoder.primitives import to_int
 from pydecoder.structs import array
 
 
-def test_array_returns_decoded_list_in_result():
-    ins = ['1', '2', '3']
+NOT_LIST = recursive(
+    none() |
+    text() |
+    booleans() |
+    integers() |
+    floats() |
+    complex_numbers(),
+    lambda children: dictionaries(text(), children)
+)
+
+
+@given(lists(integers()))
+def test_array_returns_decoded_list_in_result(ins):
 
     rv = array(to_int, ins)
 
@@ -20,10 +45,17 @@ def test_array_returns_decoded_list_in_result():
         assert res == int(orig)
 
 
-def test_stop_on_first_error_and_returns_it():
-    ins = ['1', None, '3']
-
+@given(lists(one_of(integers(), none()), min_size=1).filter(lambda x: None in x))
+def test_stop_on_first_error_and_returns_it(ins):
     rv = array(to_int, ins)
+
+    assert is_error(rv)
+    assert isinstance(rv.value, string_types)
+
+
+@given(NOT_LIST)
+def test_array_returns_error_if_value_isnt_valid_list(val):
+    rv = array(to_int, val)
 
     assert is_error(rv)
     assert isinstance(rv.value, string_types)
