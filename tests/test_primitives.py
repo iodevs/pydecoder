2d44fc05d0bea73b3996bf9bd5411718290bcd78
Jindrich K. Smitka <jsmitka@smita.info>
2017-10-09 16:16:39 +0200

    Fixed decode only string

--- tests/test_primitives.py
+++ tests/test_primitives.py
@@ -1,12 +1,43 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring, redefined-outer-name
 
-from pyresult import is_error
+from hypothesis import given
+from hypothesis.strategies import (
+    lists,
+    none,
+    integers,
+    recursive,
+    text,
+    booleans,
+    floats,
+    complex_numbers,
+    dictionaries
+)
+from pyresult import is_error, is_ok, value
 
 from pydecoder.primitives import to_string
 
 
-def test_to_string_return_error_if_value_is_none():
-    rv = to_string(None)
+NOT_STRING = recursive(
+    none() |
+    booleans() |
+    integers() |
+    floats() |
+    complex_numbers(),
+    lambda children: lists(children) | dictionaries(text(), children)
+)
+
+
+@given(NOT_STRING)
+def test_to_string_return_error_if_value_isnt_string(val):
+    rv = to_string(val)
 
     assert is_error(rv)
+
+
+@given(text())
+def test_to_string_returns_ok_result(val):
+    rv = to_string(val)
+
+    assert is_ok(rv)
+    assert value(rv) == val
